0181bb14b2989b0e5ddbe01ac099906e1731c18c
Fourthbus <clock@styc.co.uk>
2024-04-12 02:19:01 +0100

    ntpsync with new drive code

--- ntpsync.py
+++ ntpsync.py
@@ -1,6 +1,8 @@
 import wifi
 import ntptime2
+import utime
 from machine import RTC
+from timeconvert import minsFrom12
 
 def printnow():
     rtc = RTC()
@@ -14,14 +16,21 @@ def updateRTC():
     for i in range(10):
         try:
             t,ms = ntptime2.time()
+            # ~10ms delay, do this to save memory
             ntptime2.settime(t)
+            tm = utime.gmtime(t)
+            # mins since 12
+            mSe = minsFrom12(tm[3],tm[4])
+            sec = tm[5]
         except:
             print ('get time error')
-            ms=-1
+            ms  = -1
+            mSe = -1
+            sec = -1
             if i == 5:
                 ntptime2.host = "pool.ntp.org"
         else:
             break
-    return ms
+    return mSe, sec, ms
     #wifi.disconnect()
     #printnow()
